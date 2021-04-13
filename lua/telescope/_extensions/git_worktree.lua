d5789993ba5184c496119dddd397fb6f0f6a3c96
Brian Ryall <brian.ryall@udri.udayton.edu>
2021-04-13 13:40:58 -0400

    rework plugin based on feedback and added delete worktree

--- lua/telescope/_extensions/git_worktree.lua
+++ lua/telescope/_extensions/git_worktree.lua
@@ -1,53 +1,61 @@
 local Job = require("plenary.job")
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
-local action_set = require "telescope.actions.set"
-local action_state = require "telescope.actions.state"
+local actions = require("telescope.actions")
+local action_set = require("telescope.actions.set")
+local action_state = require("telescope.actions.state")
 local conf = require("telescope.config").values
 local gwt = require("git-worktree")
 
-local list_worktrees = function(cb)
-	local job = Job:new({
-		"git",
-		"worktree",
-		"list",
-		on_exit = vim.schedule_wrap(function(j_self, _, _)
-			local output = j_self:result()
-			cb(output)
-		end),
-	})
-
-	job:start()
+local get_worktree_path = function(prompt_bufnr)
+    local selection = action_state.get_selected_entry(prompt_bufnr)
+    local worktree_path = nil
+    for section in selection[1]:gmatch("%[(.*)%]") do
+        worktree_path = section
+    end
+    return worktree_path
 end
 
-local telescope_git_worktree = function()
-	local cb = function(output, _)
-		if output then
+local telescope_git_worktree = function(opts)
+    opts = opts or {}
 			pickers.new({}, {
 				prompt_prefix = "Git Worktrees >",
-				finder = finders.new_table({
-					results = output,
-				}),
+                finder = finders.new_oneshot_job(
+                    vim.tbl_flatten( {
+                        "git", "worktree", "list",
+                    } ),
+                    opts
+                ),
 				sorter = conf.generic_sorter({}),
-				attach_mappings = function()
+				attach_mappings = function(_, map)
+                    -- Switch to chosen worktree
                     action_set.select:replace(function(prompt_bufnr, type)
-                        local selection = action_state.get_selected_entry(prompt_bufnr)
-                        local worktree = nil
-                        for section in selection[1]:gmatch("%[(.*)%]") do
-                            worktree = section
-                        end
-                        if worktree ~= nil then
-                            gwt.switch_worktree(worktree)
+                        local worktree_path = get_worktree_path(prompt_bufnr)
+                        actions.close(prompt_bufnr)
+                        if worktree_path ~= nil then
+                            gwt.switch_worktree(worktree_path)
                         end
                     end)
-                    --TODO add delete worktree
-                    --TODO add create worktree? is this possible?
+                    -- Delete chosen worktree
+                    map("i", "<c-d>", function(prompt_bufnr)
+                        local worktree_path = get_worktree_path(prompt_bufnr)
+                        actions.close(prompt_bufnr)
+                        if worktree_path ~= nil then
+                            gwt.delete_worktree(worktree_path)
+                        end
+                    end)
+                    -- Force delete chosen worktree
+                    map("i", "<c-D>", function(prompt_bufnr)
+                        local worktree_path = get_worktree_path(prompt_bufnr)
+                        actions.close(prompt_bufnr)
+                        if worktree_path ~= nil then
+                            gwt.delete_worktree(worktree_path, true)
+                        end
+                    end)
+                    -- TODO Create chosen worktree
 					return true
 				end,
 			}):find()
-		end
-	end
-	list_worktrees(cb)
 end
 
 return require("telescope").register_extension({
