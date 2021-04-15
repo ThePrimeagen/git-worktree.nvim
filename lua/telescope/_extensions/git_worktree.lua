f3a4bac80431c0254c91ab1ec6cc64033dc0709b
Brian Ryall <brianryall@fastmail.com>
2021-04-15 16:08:07 -0400

    cleanup telescope ext

--- lua/telescope/_extensions/git_worktree.lua
+++ lua/telescope/_extensions/git_worktree.lua
@@ -17,49 +17,57 @@ local get_worktree_path = function(prompt_bufnr)
     return rel_path:make_relative(gwt.get_root())
 end
 
+local switch_worktree = function(prompt_bufnr)
+    local worktree_path = get_worktree_path(prompt_bufnr)
+    actions.close(prompt_bufnr)
+    if worktree_path ~= nil then
+        gwt.switch_worktree(worktree_path)
+    end
+end
+
+local delete_worktree = function(prompt_bufnr, force)
+    local worktree_path = get_worktree_path(prompt_bufnr)
+    actions.close(prompt_bufnr)
+    if worktree_path ~= nil then
+        gwt.delete_worktree(worktree_path, force)
+    end
+end
+
+local create_worktree = function(prompt_bufnr)
+    local worktree_path = action_state.get_current_line()
+    actions.close(prompt_bufnr)
+end
+
 local telescope_git_worktree = function(opts)
     opts = opts or {}
     pickers.new({}, {
         prompt_prefix = "Git Worktrees >",
-        finder = finders.new_oneshot_job(vim.tbl_flatten({
-            "git",
-            "worktree",
-            "list",
-        }), opts),
+        finder = finders.new_oneshot_job(vim.tbl_flatten({"git", "worktree", "list"}),
+                                         opts),
         sorter = conf.generic_sorter({}),
         attach_mappings = function(_, map)
-            -- Switch to chosen worktree
-            action_set.select:replace(function(prompt_bufnr, _)
-                local worktree_path = get_worktree_path(prompt_bufnr)
-                actions.close(prompt_bufnr)
-                if worktree_path ~= nil then
-                    gwt.switch_worktree(worktree_path)
-                end
-            end)
-            -- Delete chosen worktree
+            action_set.select:replace(switch_worktree)
+
             map("i", "<c-d>", function(prompt_bufnr)
-                local worktree_path = get_worktree_path(prompt_bufnr)
-                actions.close(prompt_bufnr)
-                if worktree_path ~= nil then
-                    gwt.delete_worktree(worktree_path)
-                end
+                delete_worktree(prompt_bufnr)
             end)
-            -- Force delete chosen worktree
+            map("n", "<c-d>", function(prompt_bufnr)
+                delete_worktree(prompt_bufnr)
+            end)
             map("i", "<c-D>", function(prompt_bufnr)
-                local worktree_path = get_worktree_path(prompt_bufnr)
-                actions.close(prompt_bufnr)
-                if worktree_path ~= nil then
-                    gwt.delete_worktree(worktree_path, true)
-                end
+                delete_worktree(prompt_bufnr, true)
             end)
-            -- TODO Create chosen worktree
+            map("n", "<c-D>", function(prompt_bufnr)
+                delete_worktree(prompt_bufnr, true)
+            end)
+
+            map("i", "<c-e>", create_worktree)
+            map("n", "<c-e>", create_worktree)
+
             return true
-        end,
+        end
     }):find()
 end
 
-return require("telescope").register_extension({
-    exports = {
-        git_worktrees = telescope_git_worktree,
-    },
-})
+return require("telescope").register_extension(
+           {exports = {git_worktrees = telescope_git_worktree}})
