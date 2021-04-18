8b4f270827bfe4affa8a6d9b03e74e94b30d9914
Brian Ryall <brianryall@fastmail.com>
2021-04-18 11:47:17 -0400

    added prompt for path

--- lua/telescope/_extensions/git_worktree.lua
+++ lua/telescope/_extensions/git_worktree.lua
@@ -1,4 +1,5 @@
 local Path = require("plenary.path")
+local Window = require("plenary.window.float")
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local actions = require("telescope.actions")
@@ -33,6 +34,43 @@ local delete_worktree = function(prompt_bufnr, force)
     end
 end
 
+local create_worktree = function()
+    require("telescope.builtin").git_branches(
+        {
+            attach_mappings = function(_)
+
+                actions.select_default:replace(
+                    function(prompt_bufnr, _)
+                        local selected_entry = action_state.get_selected_entry()
+                        actions.close(prompt_bufnr)
+                        if selected_entry ~= nil then
+                            local branch = selected_entry.value
+
+                            local window = Window.percentage_range_window(0.5, 0.2)
+                            vim.api.nvim_buf_set_option(window.bufnr, "buftype",
+                                                        "prompt")
+                            vim.fn.prompt_setprompt(window.bufnr, "Worktree Location: ")
+                            vim.fn.prompt_setcallback(window.bufnr, function(text)
+                                vim.api.nvim_win_close(window.win_id, true)
+                                vim.api.nvim_buf_delete(window.bufnr, {force = true})
+                                if text ~= "" then
+                                    gwt.create_worktree(text, branch)
+                                else
+                                    print("No path to create worktree")
+                                end
+                            end)
+                            vim.cmd [[startinsert]]
+
+                        end
+                    end)
+
+                -- do we need to replace other default maps?
+
+                return true
+            end
+        })
+end
+
 local telescope_git_worktree = function(opts)
     opts = opts or {}
     pickers.new({}, {
@@ -61,35 +99,10 @@ local telescope_git_worktree = function(opts)
     }):find()
 end
 
-local telescope_create_git_worktree = function(opts)
-    opts = opts or {}
-
-    require("telescope.builtin").git_branches(
-        {
-            attach_mappings = function(_)
-
-                actions.select_default:replace(
-                    function(prompt_bufnr, _)
-                        local selected_entry = action_state.get_selected_entry()
-                        actions.close(prompt_bufnr)
-                        if selected_entry ~= nil then
-                            local branch = selected_entry.value
-                            local path = branch:gsub("/", "-")
-                            gwt.create_worktree(path, branch)
-                        end
-                    end)
-
-                -- do we need to replace other default maps?
-
-                return true
-            end
-        })
-end
-
 return require("telescope").register_extension(
            {
         exports = {
             git_worktrees = telescope_git_worktree,
-            create_git_worktree = telescope_create_git_worktree
+            create_git_worktree = create_worktree
         }
     })
