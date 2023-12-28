96fdc1b900b982fa3aaaa8657b58818b7d75917f
Brian Ryall <me@brianryall.xyz>
2023-12-27 23:05:45 -0500

    ci: switch to vusted for testing

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    neorocks. url = "github:nvim-neorocks/neorocks";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
@@ -56,28 +55,16 @@
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [
-            inputs.neorocks.overlays.default
           ];
         };
         devShells = {
           default = pkgs.mkShell {
             name = "haskell-tools.nvim-shell";
             inherit (pre-commit-check) shellHook;
-            buildInputs =
-              with pkgs; [
-                neorocks
-                haskellPackages.neolua-bin
-                stylua
-              ]
-              #++ (with inputs.pre-commit-hooks.packages.${system}; [
-              #  alejandra
-              #  lua-language-server
-              #  stylua
-              #  luacheck
-              #  markdownlint-cli
-              #])
-              ;
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [];
+            buildInputs = with pkgs; [
+              luajitPackages.vusted
+              stylua
+            ];
           };
         };
 
@@ -113,18 +100,6 @@
             inherit (config.packages) neodev-plugin telescope-plugin;
             inherit (inputs) pre-commit-hooks;
             inherit self;
-          };
-          neorocks-test-stable = pkgs.callPackage ./nix/neorocks-test.nix {
-            name = "git-worktree-stable";
-            inherit self;
-            nvim = pkgs.neovim-unwrapped;
-            inherit (config.packages) plenary-plugin;
-          };
-          neorocks-test-unstable = pkgs.callPackage ./nix/neorocks-test.nix {
-            name = "git-worktree-nightly";
-            inherit self;
-            nvim = pkgs.neovim-nightly;
-            inherit (config.packages) plenary-plugin;
           };
         };
       };
